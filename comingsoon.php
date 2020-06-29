84d03beea2cd3c9ad6c358adf02c236ed30c359b
Bola <bolanaguib@gmail.com>
2020-06-30 01:23:33 +0200

    [ added ] index, header , footer , comingsoon php files

---
+++ comingsoon.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Coming Soon Template */
+get_header(); ?>
+<!-- START container -->
+<div class='container mx-auto flex h-screen'>
+    <div class="content-center text-center my-auto mx-auto ">
+        <img class="inline-block" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white.png" alt="">
+        <span class="block text-white">coming soon</span>
+    </div>
+
+</div>
+<!-- END container -->
+<?php get_footer(); ?>

63176adabcdade6cd46f23b4f7b5488edc0447be
Bola <bolanaguib@gmail.com>
2020-07-03 02:36:51 +0200

    [ added ] products block

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 function add_enqueue_styles()
 {
     wp_enqueue_style('theme_style',  get_template_directory_uri() . '/style.min.css', 1);
-    // wp_enqueue_style('uikit_css',  'https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/css/uikit.min.css');
-    // wp_enqueue_style('google_font',  'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700;800&family=Poppins:wght@400;500;600;800&display=swap');
+    wp_enqueue_style('google_font',  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap');
+    wp_enqueue_style('material_icons',  'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css');
     // wp_enqueue_script('uikit_js', 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js', array(), false, true);
 
 }

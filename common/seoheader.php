d186a0c660f059035006a958af62989061bbf2d6
Bola <bolanaguib@gmail.com>
2020-07-05 03:30:25 +0200

    [ Added ] SEO HEADERS

---
+++ common/seoheader.php
@@ -0,0 +1,4 @@
+<title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name') ?> </title>
+<meta name="title" content=" <?php the_field('meta_title'); ?> " />
+<meta name="description" content=" <?php the_field('meta_description'); ?> " />
+<meta name="keywords" content=" <?php the_field('meta_keywords'); ?> " />
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YumYumsGallery - Coming Soon </title>
+    <?php get_template_part('common/seoheader'); ?>
     <?php wp_head(); ?>
 </head>
 

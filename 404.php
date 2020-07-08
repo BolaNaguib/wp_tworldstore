a66b1834893079fb5df7cee179f7673891310a68
Bola <bolanaguib@gmail.com>
2020-07-08 09:06:27 +0200

    [ Added ] 404 Page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<!--  home page  -->
+
+
+<div class="text-center py-24">
+    <h1 class="text-6xl text-secondary">404</h1>
+    <span class="text-gray-600 text-xl lg:text-4xl">Ooops....the page you requested is not available</span>
+    <br>
+    <span class="text-xl lg:text-4xl">feel free to go to <a class="transition duration-300 ease-in-out text-main hover:text-secondary" href="<?php echo home_url(); ?>">Home Page </a> or check our <a class="transition duration-300 ease-in-out text-main hover:text-secondary" href="<?php echo home_url(); ?>/shop"> Shop </a> </span>
+</div>
+
+
+
+</div>
+<!-- END container mx-auto -->
+
+
+
+
+
+
+<?php get_footer(); ?>

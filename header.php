75862b1504910afc8fb529f73dc128593b2732f1
Bola <bolanaguib@gmail.com>
2020-07-08 11:26:26 +0200

    [ Activated ] Searchform

--- header.php
+++ header.php
@@ -137,14 +137,19 @@ $header_js = get_field('header_js', 'options');
 
                     </div>
                 </div>
-                <div class="w-auto h-10 pl-3 pr-2 bg-white border rounded-full flex justify-between items-center relative">
-                    <input type="search" name="search" id="search" placeholder="Search" class="appearance-none w-full outline-none focus:outline-none active:outline-none" />
-                    <button type="submit" class="ml-1 outline-none focus:outline-none active:outline-none">
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
-                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
-                    </button>
-                </div>
+                <form method="get" action="<?php echo home_url('/'); ?>" role="search">
+
+                    <div class="w-auto h-10 pl-3 pr-2 bg-white border rounded-full flex justify-between items-center relative">
+
+                        <input placeholder="<?php echo esc_attr_x('Search …', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" type="search" id="search" class="appearance-none w-full outline-none focus:outline-none active:outline-none" />
+                        <input type="hidden" name="post_type" value="product" />
+                        <button type="submit" class="ml-1 outline-none focus:outline-none active:outline-none">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
+                                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </form>
 
 
             </nav>

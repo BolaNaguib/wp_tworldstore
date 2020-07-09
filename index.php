78d06a9965e5c0b579552134d8da8eabcc1b5d29
Bola <bolanaguib@gmail.com>
2020-07-10 01:46:19 +0200

    [ added ] Styling

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $queried_object = get_queried_object();
 ?>
 <!-- START container mx-auto bg-white -->
-<div class='pb-6'>
+<div class='pb-6 mb-6'>
     <div class="xx rounded text-white p-6 py-20 text-center bg-fixed bg-cover relative shadow-lg overflow-hidden" style=" 
     background: url('https://www.inquirer.com/resizer/JmbnMe9pQTBvgUpraYaR6YBE5f8=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/KDHKAOOKANC2LHCOA3UNRVNQOE.jpg');
     background-attachment: fixed;
@@ -12,7 +12,7 @@ $queried_object = get_queried_object();
 	background-repeat: no-repeat;
 	background-position: center;
 ">
-        <h1 class="text-6xl z-10 relative"> <?php woocommerce_page_title(); ?> </h1>
+        <h1 class="text-6xl z-10 relative"> <?php the_title(); ?> </h1>
         <p class="z-10 relative"><?php echo $queried_object->description ?></p>
 
     </div>

2843b0c4eab2f19219eab8d8f670de02cb5ab547
Bola <bolanaguib@gmail.com>
2020-07-12 16:47:15 +0200

    [ added ] margin to next product

--- functions.php
+++ functions.php
@@ -49,7 +49,7 @@ add_action('wp_enqueue_scripts', 'add_enqueue_styles', 10);
 function bbloomer_next_product()
 {
     ob_start();
-    echo '<div class="next-button rounded-full bg-white inline-block text-main hover:text-secondary transition duration-300 ease-in-out  fixed bottom-0 left-0 m-6 z-10">';
+    echo '<div class="next-button mb-24 rounded-full bg-white inline-block text-main hover:text-secondary transition duration-300 ease-in-out  fixed bottom-0 left-0 m-6 z-10">';
     // 'product_cat' will make sure to return next/prev from current category
     $next = previous_post_link('%link', '<svg  style="width:40px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-circle-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-arrow-circle-left fa-w-16"><path fill="currentColor" d="M256 504C119 504 8 393 8 256S119 8 256 8s248 111 248 248-111 248-248 248zm28.9-143.6L209.4 288H392c13.3 0 24-10.7 24-24v-16c0-13.3-10.7-24-24-24H209.4l75.5-72.4c9.7-9.3 9.9-24.8.4-34.3l-11-10.9c-9.4-9.4-24.6-9.4-33.9 0L107.7 239c-9.4 9.4-9.4 24.6 0 33.9l132.7 132.7c9.4 9.4 24.6 9.4 33.9 0l11-10.9c9.5-9.5 9.3-25-.4-34.3z" class=""></path></svg>', true, ' ', 'product_cat');
     echo $next;
@@ -62,7 +62,7 @@ add_shortcode('woo-prod-next', 'bbloomer_next_product');
 function bbloomer_previous_product()
 {
     ob_start();
-    echo '<div class="previous-button rounded-full bg-white  inline-block text-main hover:text-secondary transition duration-300 ease-in-out  fixed bottom-0 right-0 m-6 z-10">';
+    echo '<div class="previous-button mb-24 rounded-full bg-white  inline-block text-main hover:text-secondary transition duration-300 ease-in-out  fixed bottom-0 right-0 m-6 z-10">';
     // 'product_cat' will make sure to return next/prev from current category
     $previous = next_post_link('%link', '<svg style="width:40px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-circle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-arrow-circle-right fa-w-16"><path fill="currentColor" d="M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zm-28.9 143.6l75.5 72.4H120c-13.3 0-24 10.7-24 24v16c0 13.3 10.7 24 24 24h182.6l-75.5 72.4c-9.7 9.3-9.9 24.8-.4 34.3l11 10.9c9.4 9.4 24.6 9.4 33.9 0L404.3 273c9.4-9.4 9.4-24.6 0-33.9L271.6 106.3c-9.4-9.4-24.6-9.4-33.9 0l-11 10.9c-9.5 9.6-9.3 25.1.4 34.4z" class=""></path></svg>', true, ' ', 'product_cat');
     echo $previous;

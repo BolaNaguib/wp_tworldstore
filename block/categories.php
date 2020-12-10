98c0c225d4db54d8d81431de76711b8bdf6f3a1f
Bola <bolanaguib@gmail.com>
2020-12-10 18:05:20 +0200

    [ Added ] Categories Block

--- block/categories.php
+++ block/categories.php
@@ -1,33 +1,30 @@
+<?php
+
+$column = get_field('columns') ? get_field('columns') : '3';
+?>
 <!-- START container mx-auto -->
 <div class='container mx-auto bg-white '>
 
-    <div class="grid grid-cols-2 gap-2 md:grid-cols-3 p-2 pb-0">
-        <?php for ($i = 0; $i < 3; $i++) : ?>
-            <a href="#" class="rounded text-white group  text-center bg-fixed bg-cover relative shadow-lg overflow-hidden">
-                <img class="object-cover h-full w-full group-hover:scale-125 transform transition duration-300 ease-in-out rounded overflow-hidden" src="https://www.inquirer.com/resizer/JmbnMe9pQTBvgUpraYaR6YBE5f8=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/KDHKAOOKANC2LHCOA3UNRVNQOE.jpg" alt="">
-                <span class="text-small lg:text-l z-10 absolute left-0 bottom-0 ml-2 mb-2 xx text-white rounded p-2 px-4"> <?php woocommerce_page_title(); ?> </span>
-                <p class="z-10 relative"><?php echo $queried_object->description ?></p>
+    <div class="grid grid-cols-2 gap-2 md:grid-cols-<?php echo $column ?> p-2 pb-0">
+        <?php if (have_rows('categories')) : ?>
+            <?php while (have_rows('categories')) : the_row();
+                //ACF Fields
+                $term = get_sub_field('category');
+                $category_image = get_sub_field('category_image');
+                $img = $category_image ? $category_image['url'] : 'https://www.inquirer.com/resizer/JmbnMe9pQTBvgUpraYaR6YBE5f8=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/KDHKAOOKANC2LHCOA3UNRVNQOE.jpg';
+            ?>
+                <a href="<?php echo get_term_link($term[0]->term_id) ?>" class="rounded text-white group  text-center bg-fixed bg-cover relative shadow-lg overflow-hidden">
+                    <img class="object-cover h-full w-full group-hover:scale-125 transform transition duration-300 ease-in-out rounded overflow-hidden" src="<?php echo $img ?>" alt="<?php echo $category_image['alt'] ?>" title="<?php echo $category_image['title'] ?>">
+                    <span class="text-small lg:text-l z-10 absolute left-0 bottom-0 ml-2 mb-2 xx text-white rounded p-2 px-4">
+                        <?php echo $term[0]->name ?>
 
-            </a>
-        <?php endfor; ?>
-    </div>
-    <div class="grid grid-cols-2 gap-2 md:grid-cols-2 p-2 pb-0">
-        <?php for ($i = 0; $i < 2; $i++) : ?>
-            <a href="#" class="rounded text-white group  text-center bg-fixed bg-cover relative shadow-lg overflow-hidden">
-                <img class="object-cover h-full w-full group-hover:scale-125 transform transition duration-300 ease-in-out rounded overflow-hidden" src="https://www.inquirer.com/resizer/JmbnMe9pQTBvgUpraYaR6YBE5f8=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/KDHKAOOKANC2LHCOA3UNRVNQOE.jpg" alt="">
-                <span class="text-small lg:text-l z-10 absolute left-0 bottom-0 ml-2 mb-2 xx text-white rounded p-2 px-4"> <?php woocommerce_page_title(); ?> </span>
-                <p class="z-10 relative"><?php echo $queried_object->description ?></p>
+                    </span>
 
-            </a>
-        <?php endfor; ?>
-    </div>
-    <div class="grid grid-cols-2 gap-2 md:grid-cols-4 p-2 pb-0">
-        <?php for ($i = 0; $i < 4; $i++) : ?>
-            <a href="#" class="rounded text-white group  text-center bg-fixed bg-cover relative shadow-lg overflow-hidden">
-                <img class="object-cover h-full w-full group-hover:scale-125 transform transition duration-300 ease-in-out rounded overflow-hidden" src="https://www.inquirer.com/resizer/JmbnMe9pQTBvgUpraYaR6YBE5f8=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/KDHKAOOKANC2LHCOA3UNRVNQOE.jpg" alt="">
-                <span class="text-small lg:text-l z-10 absolute left-0 bottom-0 ml-2 mb-2 xx text-white rounded p-2 px-4"> <?php woocommerce_page_title(); ?> </span>
-                <p class="z-10 relative"><?php echo $queried_object->description ?></p>
+                </a>
+            <?php endwhile; ?>
+        <?php endif; ?>
 
-            </a>
-        <?php endfor; ?>
     </div>
+
+
+    <div class="grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-1  "></div>

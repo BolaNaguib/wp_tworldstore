20a77aad2987429f9ceee8bfa139cae404a1ae3f
Bola <bolanaguib@gmail.com>
2020-12-23 17:10:17 +0200

    [ Fixed ] Contacts

--- block/contactform.php
+++ block/contactform.php
@@ -1,33 +1,64 @@
+<?php
+
+$col1_icon = get_field('col1_icon') ? get_field('col1_icon') : '<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="share-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-share-alt fa-w-14">
+<path fill="currentColor" d="M352 320c-25.6 0-48.9 10-66.1 26.4l-98.3-61.5c5.9-18.8 5.9-39.1 0-57.8l98.3-61.5C303.1 182 326.4 192 352 192c53 0 96-43 96-96S405 0 352 0s-96 43-96 96c0 9.8 1.5 19.6 4.4 28.9l-98.3 61.5C144.9 170 121.6 160 96 160c-53 0-96 43-96 96s43 96 96 96c25.6 0 48.9-10 66.1-26.4l98.3 61.5c-2.9 9.4-4.4 19.1-4.4 28.9 0 53 43 96 96 96s96-43 96-96-43-96-96-96zm0-272c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM96 304c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm256 160c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48z" class=""></path>
+</svg>';
+$col1_icon_title = get_field('col1_icon_title') ? get_field('col1_icon_title') : 'hey';
+$col1_icon_caption = get_field('col1_icon_caption') ? get_field('col1_icon_caption') : 'Hello World';
+$col1_title = get_field('col1_title') ? get_field('col1_title') : 'More About Us';
+$col1_caption = get_field('col1_caption') ? get_field('col1_caption') : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi magni, minus dolorum dicta reiciendis consequatur in repellendus voluptates inventore voluptatum aut ratione autem qui perferendis aliquam laudantium! Cumque, reiciendis sunt.';
+$col1_form_title = get_field('col1_form_title') ? get_field('col1_form_title') : 'We are happy to talk to you';
+$col1_form = get_field('col1_form') ? get_field('col1_form') : '[contact-form-7 id="27" title="Contact form 1"]';
+
+$col2_icon = get_field('col2_icon') ? get_field('col2_icon') : '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="gift" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-gift fa-w-16">
+    <path fill="currentColor" d="M464 144h-39.3c9.5-13.4 15.3-29.9 15.3-48 0-44.1-33.4-80-74.5-80-42.3 0-66.8 25.4-109.5 95.8C213.3 41.4 188.8 16 146.5 16 105.4 16 72 51.9 72 96c0 18.1 5.8 34.6 15.3 48H48c-26.5 0-48 21.5-48 48v96c0 8.8 7.2 16 16 16h16v144c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V304h16c8.8 0 16-7.2 16-16v-96c0-26.5-21.5-48-48-48zm-187.8-3.6c49.5-83.3 66-92.4 89.3-92.4 23.4 0 42.5 21.5 42.5 48s-19.1 48-42.5 48H274l2.2-3.6zM146.5 48c23.4 0 39.8 9.1 89.3 92.4l2.1 3.6h-91.5c-23.4 0-42.5-21.5-42.5-48 .1-26.5 19.2-48 42.6-48zM192 464H80c-8.8 0-16-7.2-16-16V304h128v160zm0-192H32v-80c0-8.8 7.2-16 16-16h144v96zm96 192h-64V176h64v288zm160-16c0 8.8-7.2 16-16 16H320V304h128v144zm32-176H320v-96h144c8.8 0 16 7.2 16 16v80z" class=""></path>
+</svg>';
+$col2_icon_title = get_field('col2_icon_title') ? get_field('col2_icon_title') : 'hey';
+$col2_icon_caption = get_field('col2_icon_caption') ? get_field('col2_icon_caption') : 'Hello World';
+// $col2_title = get_field('col2_title') ? get_field('col2_title') : 'More About Us';
+// $col2_caption = get_field('col2_caption') ? get_field('col2_caption') : '';
+$col2_form_title = get_field('col2_form_title') ? get_field('col2_form_title') : 'Second Tab';
+$col2_form = get_field('col2_form') ? get_field('col2_form') : '[contact-form-7 id="27" title="Contact form 1"]';
+
+$col3_icon = get_field('col3_icon') ? get_field('col3_icon') : '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="gift" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-gift fa-w-16">
+    <path fill="currentColor" d="M464 144h-39.3c9.5-13.4 15.3-29.9 15.3-48 0-44.1-33.4-80-74.5-80-42.3 0-66.8 25.4-109.5 95.8C213.3 41.4 188.8 16 146.5 16 105.4 16 72 51.9 72 96c0 18.1 5.8 34.6 15.3 48H48c-26.5 0-48 21.5-48 48v96c0 8.8 7.2 16 16 16h16v144c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V304h16c8.8 0 16-7.2 16-16v-96c0-26.5-21.5-48-48-48zm-187.8-3.6c49.5-83.3 66-92.4 89.3-92.4 23.4 0 42.5 21.5 42.5 48s-19.1 48-42.5 48H274l2.2-3.6zM146.5 48c23.4 0 39.8 9.1 89.3 92.4l2.1 3.6h-91.5c-23.4 0-42.5-21.5-42.5-48 .1-26.5 19.2-48 42.6-48zM192 464H80c-8.8 0-16-7.2-16-16V304h128v160zm0-192H32v-80c0-8.8 7.2-16 16-16h144v96zm96 192h-64V176h64v288zm160-16c0 8.8-7.2 16-16 16H320V304h128v144zm32-176H320v-96h144c8.8 0 16 7.2 16 16v80z" class=""></path>
+</svg>';
+$col3_icon_title = get_field('col3_icon_title') ? get_field('col3_icon_title') : 'hey';
+$col3_icon_caption = get_field('col3_icon_caption') ? get_field('col3_icon_caption') : 'Hello World';
+// $col3_title = get_field('col3_title') ? get_field('col3_title') : 'More About Us';
+// $col3_caption = get_field('col3_caption') ? get_field('col3_caption') : '';
+$col3_form_title = get_field('col3_form_title') ? get_field('col3_form_title') : 'Follow Us On Social Media';
+$col3_form = get_field('col3_form') ? get_field('col3_form') : '';
+
+
+$fb = get_field('facebook', 'option') ? get_field('facebook', 'option') : '#';
+$ig = get_field('instagram', 'option') ? get_field('instagram', 'option') : '#';
+?>
+
 <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
     <div id="tabOneBlock" class="text-center mb-6">
         <div class="qq mx-2 transition duration-300 ease-in-out text-white p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-4 shadow-lg  rounded-full bg-main hover:underline hover:bg-secondary">
-            <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="share-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-share-alt fa-w-14">
-                <path fill="currentColor" d="M352 320c-25.6 0-48.9 10-66.1 26.4l-98.3-61.5c5.9-18.8 5.9-39.1 0-57.8l98.3-61.5C303.1 182 326.4 192 352 192c53 0 96-43 96-96S405 0 352 0s-96 43-96 96c0 9.8 1.5 19.6 4.4 28.9l-98.3 61.5C144.9 170 121.6 160 96 160c-53 0-96 43-96 96s43 96 96 96c25.6 0 48.9-10 66.1-26.4l98.3 61.5c-2.9 9.4-4.4 19.1-4.4 28.9 0 53 43 96 96 96s96-43 96-96-43-96-96-96zm0-272c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM96 304c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm256 160c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48z" class=""></path>
-            </svg>
+            <?php echo $col1_icon ?>
         </div>
 
-        <h2 class=""> hey </h2>
-        <span class="text-main"> Hello world </span>
+        <h2 class=""> <?php echo $col1_icon_title ?> </h2>
+        <span class="text-main"> <?php echo $col1_icon_caption ?> </span>
     </div>
     <div id="tabTwoBlock" class="text-center mb-6">
         <div class="qq mx-2 transition duration-300 ease-in-out text-white p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-4 shadow-lg  rounded-full bg-main hover:underline hover:bg-secondary">
-            <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="gift" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-gift fa-w-16">
-                <path fill="currentColor" d="M464 144h-39.3c9.5-13.4 15.3-29.9 15.3-48 0-44.1-33.4-80-74.5-80-42.3 0-66.8 25.4-109.5 95.8C213.3 41.4 188.8 16 146.5 16 105.4 16 72 51.9 72 96c0 18.1 5.8 34.6 15.3 48H48c-26.5 0-48 21.5-48 48v96c0 8.8 7.2 16 16 16h16v144c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V304h16c8.8 0 16-7.2 16-16v-96c0-26.5-21.5-48-48-48zm-187.8-3.6c49.5-83.3 66-92.4 89.3-92.4 23.4 0 42.5 21.5 42.5 48s-19.1 48-42.5 48H274l2.2-3.6zM146.5 48c23.4 0 39.8 9.1 89.3 92.4l2.1 3.6h-91.5c-23.4 0-42.5-21.5-42.5-48 .1-26.5 19.2-48 42.6-48zM192 464H80c-8.8 0-16-7.2-16-16V304h128v160zm0-192H32v-80c0-8.8 7.2-16 16-16h144v96zm96 192h-64V176h64v288zm160-16c0 8.8-7.2 16-16 16H320V304h128v144zm32-176H320v-96h144c8.8 0 16 7.2 16 16v80z" class=""></path>
-            </svg>
+            <?php echo $col2_icon ?>
         </div>
 
-        <h2 class=""> hey </h2>
-        <span class="text-main"> Hello world </span>
+        <h2 class=""> <?php echo $col2_icon_title ?> </h2>
+        <span class="text-main"> <?php echo $col2_icon_caption ?> </span>
     </div>
     <div id="tabThreeBlock" class="text-center mb-6">
         <div class="qq mx-2 transition duration-300 ease-in-out text-white p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-4 shadow-lg  rounded-full bg-main hover:underline hover:bg-secondary">
-            <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="gift" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-gift fa-w-16">
-                <path fill="currentColor" d="M464 144h-39.3c9.5-13.4 15.3-29.9 15.3-48 0-44.1-33.4-80-74.5-80-42.3 0-66.8 25.4-109.5 95.8C213.3 41.4 188.8 16 146.5 16 105.4 16 72 51.9 72 96c0 18.1 5.8 34.6 15.3 48H48c-26.5 0-48 21.5-48 48v96c0 8.8 7.2 16 16 16h16v144c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V304h16c8.8 0 16-7.2 16-16v-96c0-26.5-21.5-48-48-48zm-187.8-3.6c49.5-83.3 66-92.4 89.3-92.4 23.4 0 42.5 21.5 42.5 48s-19.1 48-42.5 48H274l2.2-3.6zM146.5 48c23.4 0 39.8 9.1 89.3 92.4l2.1 3.6h-91.5c-23.4 0-42.5-21.5-42.5-48 .1-26.5 19.2-48 42.6-48zM192 464H80c-8.8 0-16-7.2-16-16V304h128v160zm0-192H32v-80c0-8.8 7.2-16 16-16h144v96zm96 192h-64V176h64v288zm160-16c0 8.8-7.2 16-16 16H320V304h128v144zm32-176H320v-96h144c8.8 0 16 7.2 16 16v80z" class=""></path>
-            </svg>
+            <?php echo $col3_icon ?>
         </div>
 
-        <h2 class=""> hey </h2>
-        <span class="text-main"> Hello world </span>
+        <h2 class=""> <?php echo $col3_icon_title ?> </h2>
+        <span class="text-main"> <?php echo $col3_icon_caption ?> </span>
     </div>
 </div>
 <hr class="my-4 block">
@@ -42,34 +73,35 @@
 
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
     <div>
-        <h3 class="text-2xl text-main"> More About Us </h3>
+        <h3 class="text-2xl text-main"> <?php echo $col1_title ?></h3>
         <hr class="my-4 block">
 
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi magni, minus dolorum dicta reiciendis consequatur in repellendus voluptates inventore voluptatum aut ratione autem qui perferendis aliquam laudantium! Cumque, reiciendis sunt.
+            <?php echo $col1_caption ?>
+
         </p>
     </div>
     <div>
         <div id="tabOneForm" style="">
-            <h3 class="text-2xl text-main"> We are happy to talk to you </h3>
+            <h3 class="text-2xl text-main"> <?php echo $col1_form_title ?> </h3>
             <hr class="my-4 block">
-            <?php echo do_shortcode('[contact-form-7 id="27" title="Contact form 1"]') ?>
+            <?php echo do_shortcode('' . $col1_form . '') ?>
         </div>
         <div id="tabTwoForm" style="display: none;">
-            <h3 class="text-2xl text-main"> Second Tab </h3>
+            <h3 class="text-2xl text-main"> <?php echo $col2_form_title ?> </h3>
             <hr class="my-4 block">
-            <?php echo do_shortcode('[contact-form-7 id="27" title="Contact form 1"]') ?>
+            <?php echo do_shortcode('' . $col2_form . '') ?>
         </div>
         <div id="tabThreeForm" style="display: none;">
-            <h3 class="text-2xl text-main"> Follow Us On Social Media </h3>
+            <h3 class="text-2xl text-main"> <?php echo $col3_form_title ?> </h3>
             <hr class="my-4 block">
-            <a href="#" class="text-gray-600 hover:text-white bg-gray-100 hover:bg-main px-2 py-4 block qq shadow-sm 
+            <a href="<?php echo $fb ?>" class="text-gray-600 hover:text-white bg-gray-100 hover:bg-main px-2 py-4 block qq shadow-sm 
             transition duration-300 ease-in-out rounded mb mb-4">
                 <svg style="" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-facebook-f fa-w-10">
                     <path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" class=""></path>
                 </svg>
             </a>
-            <a href="#" class="text-gray-600 hover:text-white bg-gray-100 hover:bg-main px-2 py-4 block qq shadow-sm 
+            <a href="<?php echo $ig ?>" class="text-gray-600 hover:text-white bg-gray-100 hover:bg-main px-2 py-4 block qq shadow-sm 
             transition duration-300 ease-in-out rounded">
                 <svg style="" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-instagram fa-w-14">
                     <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" class=""></path>
